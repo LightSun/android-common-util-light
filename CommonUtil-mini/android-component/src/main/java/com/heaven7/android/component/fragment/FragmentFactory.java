1c4db7fefd15c8306798ab92a6bd1058691c130c
LightSun <532278976@qq.com>
2016-05-25 17:33:55 +0800

    add method for FragmentFactory

--- CommonUtil-mini/android-component/src/main/java/com/heaven7/android/component/fragment/FragmentFactory.java
+++ CommonUtil-mini/android-component/src/main/java/com/heaven7/android/component/fragment/FragmentFactory.java
@@ -1,20 +1,27 @@
 package com.heaven7.android.component.fragment;
 
-import android.support.v4.app.Fragment;
-
 /**
  * Created by heaven7 on 2016/5/24.
  */
 public final class FragmentFactory {
 
-       public  static  Fragment newFragment(FragmentHelper helper){
-           BaseFragment fragment = new BaseFragment();
-           final FragmentMediator mediator = fragment.getFragmentMediator();
-           mediator.init(helper.getLayoutId(), helper.getArguments());
-           mediator.getFragmentCallbacks().addFragmentCallbacks(helper.getFragmentCallbacks());
-           if(helper.getMenuCallback()!=null) {
-               mediator.setMenuCallback(helper.getMenuCallback());
-           }
-           return  fragment;
-       }
+    public static BaseFragment newFragment(FragmentHelper helper) {
+        return newFragment(helper, BaseFragment.class);
+    }
+
+    public static <T extends BaseFragment> T newFragment(FragmentHelper helper, Class<T> clazz) {
+        T fragment;
+        try {
+            fragment = clazz.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        final FragmentMediator mediator = fragment.getFragmentMediator();
+        mediator.init(helper.getLayoutId(), helper.getArguments());
+        mediator.getFragmentCallbacks().addFragmentCallbacks(helper.getFragmentCallbacks());
+        if (helper.getMenuCallback() != null) {
+            mediator.setMenuCallback(helper.getMenuCallback());
+        }
+        return fragment;
+    }
 }
