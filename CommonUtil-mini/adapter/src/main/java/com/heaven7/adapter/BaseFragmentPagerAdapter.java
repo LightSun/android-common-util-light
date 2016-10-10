d3384bae119dd3cbc86a057647e8e901fd5bc8f3
LightSun <532278976@qq.com>
2016-10-10 17:49:50 +0800

    update BaseFragamentPagerAdapter to support transaction

--- CommonUtil-mini/adapter/src/main/java/com/heaven7/adapter/BaseFragmentPagerAdapter.java
+++ CommonUtil-mini/adapter/src/main/java/com/heaven7/adapter/BaseFragmentPagerAdapter.java
@@ -39,6 +39,7 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
 
     private List<FragmentData> mTempList;
     private IPageChangeListener mListener;
+    private boolean mBegined;
 
     public BaseFragmentPagerAdapter(@NonNull FragmentManager fm, @NonNull List<FragmentData> fragmentDatas) {
         this.mFragmentManager = fm;
@@ -70,6 +71,7 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
      * @since 1.6.2
      */
     public void sort(Comparator<FragmentData> comparator) {
+        checkTransaction();
         saveOldOrder();
         if (comparator == null) {
             Collections.sort(mFragmentDatas);
@@ -127,13 +129,20 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
      * @since 1.6.2
      */
     public void addFragmentData(FragmentData data) {
-        if (!mFragmentDatas.contains(data)) {
-            mFragmentDatas.add(data);
-            if (mListener != null) {
-                mListener.onAdd(this, data, mFragmentDatas.size() - 1);
-            }
-            notifyDataSetChanged();
-        }
+        addFragmentDataImpl(getCount(), data, true);
+    }
+
+    /**
+     * use {@link #addFragmentData(int, FragmentData)} instead.
+     * add a FragmentData by target position.
+     *
+     * @param data     the fragment data
+     * @param position the position to insert/add
+     * @since 1.6.2
+     */
+    @Deprecated
+    public void addFragmentData(FragmentData data, int position) {
+        addFragmentDataImpl(position, data , true);
     }
 
     /**
@@ -143,22 +152,19 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
      * @param position the position to insert/add
      * @since 1.6.2
      */
-    public void addFragmentData(FragmentData data, int position) {
-        if (position < 0 || position > mFragmentDatas.size()) {
-            throw new IllegalArgumentException();
-        }
-        if (!mFragmentDatas.contains(data)) {
-            mFragmentDatas.add(position, data);
-            if (mListener != null) {
-                mListener.onAdd(this, data, position);
-                for (int i = position + 1, size = mFragmentDatas.size(); i < size; i++) {
-                    mListener.onPositionChanged(this, mFragmentDatas.get(i), i - 1, i);
-                }
-            }
-            notifyDataSetChanged();
-        }
+    public void addFragmentData(int position, FragmentData data){
+        addFragmentDataImpl(position, data , true);
     }
-
+    /**
+     * add  FragmentData
+     *
+     * @param datas the fragment datas
+     * @param position the position to add
+     * @since 1.6.2
+     */
+    public void addFragmentData(int position ,List<FragmentData> datas) {
+        addFragmentDataImpl(position, datas, true);
+    }
     /**
      * add  FragmentData
      *
@@ -166,37 +172,18 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
      * @since 1.6.2
      */
     public void addFragmentData(List<FragmentData> datas) {
-        if (datas == null) {
-            throw new NullPointerException();
-        }
-        final List<FragmentData> mFragmentDatas = this.mFragmentDatas;
-        final IPageChangeListener l = this.getOnPagerChangeListener();
-        for (FragmentData data : datas) {
-            if (!mFragmentDatas.contains(data)) {
-                mFragmentDatas.add(data);
-                if (l != null) {
-                    l.onAdd(this, data, mFragmentDatas.size() - 1);
-                }
-            }
-        }
-        notifyDataSetChanged();
+        addFragmentDataImpl(getCount(), datas, true);
     }
 
+
     /**
-     * remove a FragmentData
+     * remove a FragmentData add notify
      *
      * @param data the fragment data
      * @since 1.6.2
      */
     public void removeFragmentData(FragmentData data) {
-        final int index = mFragmentDatas.indexOf(data);
-        if (index != -1) {
-            mFragmentDatas.remove(index);
-            if (mListener != null) {
-                mListener.onRemove(this, data);
-            }
-            notifyDataSetChanged();
-        }
+        removeFragmentDataImpl(data , true);
     }
 
     /**
@@ -206,18 +193,7 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
      * @since 1.6.2
      */
     public void removeFragmentData(List<FragmentData> datas) {
-        if (datas == null) {
-            throw new NullPointerException();
-        }
-        final List<FragmentData> mFragmentDatas = this.mFragmentDatas;
-        final IPageChangeListener l = this.getOnPagerChangeListener();
-        for (FragmentData data : datas) {
-            mFragmentDatas.remove(data);
-            if (l != null) {
-                l.onRemove(this, data);
-            }
-        }
-        notifyDataSetChanged();
+       removeFragmentDataImpl(datas, true);
     }
 
     /**
@@ -229,6 +205,81 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
     public List<FragmentData> getFragmentDatas() {
         return mFragmentDatas;
     }
+
+    private void addFragmentDataImpl(int position, List<FragmentData> datas, boolean notify) {
+        if (datas == null || position < 0 || position > mFragmentDatas.size()) {
+            throw new IllegalArgumentException();
+        }
+        final List<FragmentData> mFragmentDatas = this.mFragmentDatas;
+        if( notify ) {
+            final IPageChangeListener l = this.getOnPagerChangeListener();
+            int index = position;
+            for (FragmentData data : datas) {
+                mFragmentDatas.add(index, data);
+                if ( l != null) {
+                    l.onAdd(this, data, index);
+                }
+                index ++ ;
+            }
+            notifyDataSetChanged();
+        }else{
+            mFragmentDatas.addAll(position, datas);
+        }
+    }
+
+    private void addFragmentDataImpl(int position, FragmentData data, boolean notify) {
+        if (position < 0 || position > mFragmentDatas.size()) {
+            throw new IllegalArgumentException();
+        }
+        mFragmentDatas.add(position, data);
+        if(notify) {
+            if (mListener != null) {
+                mListener.onAdd(this, data, position);
+                for (int i = position + 1, size = mFragmentDatas.size(); i < size; i++) {
+                    mListener.onPositionChanged(this, mFragmentDatas.get(i), i - 1, i);
+                }
+            }
+            notifyDataSetChanged();
+        }
+    }
+
+    private void removeFragmentDataImpl(FragmentData data , boolean notify) {
+        final int index = mFragmentDatas.indexOf(data);
+        if (index != -1) {
+            mFragmentDatas.remove(index);
+            if(notify) {
+                if (mListener != null) {
+                    mListener.onRemove(this, data, index);
+                }
+                notifyDataSetChanged();
+            }
+        }
+    }
+
+    private void removeFragmentDataImpl(List<FragmentData> datas, boolean notify) {
+        if (datas == null) {
+            throw new NullPointerException();
+        }
+        final List<FragmentData> mFragmentDatas = this.mFragmentDatas;
+        if(notify) {
+            final IPageChangeListener l = this.getOnPagerChangeListener();
+            for (FragmentData data : datas) {
+                int index = mFragmentDatas.indexOf(data);
+                if (index == -1)
+                    continue;
+                mFragmentDatas.remove(index);
+                if (l != null) {
+                    l.onRemove(this, data, index);
+                }
+            }
+            notifyDataSetChanged();
+        }else{
+            for (FragmentData data : datas) {
+                mFragmentDatas.remove(data);
+            }
+        }
+    }
+    //=============================================================== //
 
     @Override
     public final int getCount() {
@@ -369,6 +420,24 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
     }
 
     /**
+     * start a pager adapter data transaction . you must call {@link Transaction#commit()} after 'CRUD' operation.
+     * @return PagerAdapterTransaction
+     */
+    public PagerAdapterTransaction beginTransaction(){
+        checkTransaction();
+        mBegined = true;
+        saveOldOrder();
+        return new Transaction();
+    }
+
+    private void checkTransaction() {
+        if(mBegined){
+            throw new IllegalStateException("you must call commit() of last transaction");
+        }
+    }
+
+
+    /**
      * contains: title, fragmentClass and fragment args(arguments)
      */
     public static class FragmentData implements Comparable<FragmentData> {
@@ -392,6 +461,19 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
             return this.priority < another.priority ? -1 :
                     (this.priority == another.priority ? 0 : 1);
             //return Integer.compare(this.priority, another.priority);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            FragmentData that = (FragmentData) o;
+            return title.equals(that.title);
+        }
+
+        @Override
+        public int hashCode() {
+            return title.hashCode();
         }
     }
 
@@ -421,8 +503,9 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
          *
          * @param adapter the adapter
          * @param data    the fragment data
+         * @param pos    the position
          */
-        void onRemove(BaseFragmentPagerAdapter adapter, FragmentData data);
+        void onRemove(BaseFragmentPagerAdapter adapter, FragmentData data, int pos);
 
         /**
          * called after add a FragmentData.
@@ -442,5 +525,85 @@ public class BaseFragmentPagerAdapter extends PagerAdapter {
          * @param newPos  new position
          */
         void onPositionChanged(BaseFragmentPagerAdapter adapter, FragmentData data, int oldPos, int newPos);
+    }
+
+    private class Transaction extends PagerAdapterTransaction{
+
+        @Override
+        public PagerAdapterTransaction addFragmentData(FragmentData data) {
+            addFragmentDataImpl(getCount(), data, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction addFragmentData(int position, FragmentData data) {
+            addFragmentDataImpl(position, data, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction addFragmentData(int position, List<FragmentData> datas) {
+            addFragmentDataImpl(position, datas, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction addFragmentData(List<FragmentData> datas) {
+            addFragmentDataImpl(getCount(), datas, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction removeFragmentData(FragmentData data) {
+            removeFragmentDataImpl(data, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction removeFragmentData(List<FragmentData> datas) {
+            removeFragmentDataImpl(datas, false);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction performSort(Comparator<FragmentData> comparator, ISortPerformer performer) {
+            performer.performSort(BaseFragmentPagerAdapter.this, getFragmentDatas(), comparator);
+            return this;
+        }
+
+        @Override
+        public PagerAdapterTransaction performUpdate(IUpdatePerformer performer) {
+            performer.performUpdate(BaseFragmentPagerAdapter.this, getFragmentDatas());
+            return this;
+        }
+
+        @Override
+        public void commit() {
+            final List<FragmentData> oldList = mTempList;
+            final List<FragmentData> newList = mFragmentDatas;
+            final IPageChangeListener l = mListener;
+            //add /remove /pos_change
+            if( l != null) {
+                final BaseFragmentPagerAdapter adapter = BaseFragmentPagerAdapter.this;
+                for (int size = newList.size(), i = size - 1; i < size; i--) {
+                    final FragmentData data = newList.get(i);
+                    final int oldIndex = oldList.indexOf(data);
+                    if (oldIndex == -1) {
+                        l.onAdd(adapter, data, i);
+                    }else if(i != oldIndex){
+                        l.onPositionChanged(adapter, data, oldIndex, i);
+                    }
+                }
+                for (int size = oldList.size(), i = size - 1; i < size; i--) {
+                    final FragmentData data = oldList.get(i);
+                    final int newIndex = newList.indexOf(data);
+                    if (newIndex == -1) {
+                        l.onRemove(adapter, data, i);
+                    }
+                }
+            }
+            notifyDataSetChanged();
+            mBegined = false;
+        }
     }
 }
